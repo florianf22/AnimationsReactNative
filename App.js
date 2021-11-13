3c6d75706db51cd90188d9e4db00b743958a432b
florianf22 <g.fasieshvili@gmail.com>
2021-11-13 17:37:21 +0400

    removed comment

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-// Inspiration: https://dribbble.com/shots/14139308-Simple-Scroll-Animation
-// Illustrations by: SAMji https://dribbble.com/SAMji_illustrator
-
 import * as React from 'react';
 import { Image, Text, View } from 'react-native';
 
