7cd4ac4d4bc54c333b804bf1c8572542bee520d1
florianf22 <g.fasieshvili@gmail.com>
2021-11-16 15:05:53 +0400

    Added fonts

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import * as React from 'react';
 import { Image, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default () => {
+  const [loaded] = useFonts({
+    Mulish: require('./assets/fonts/Mulish.ttf'),
+    MulishBold: require('./assets/fonts/MulishBold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View
       style={{
