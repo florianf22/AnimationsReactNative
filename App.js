46274897d2c3e06fac26ce44a7578852c09030d3
florianf22 <g.fasieshvili@gmail.com>
2021-11-16 17:35:00 +0400

    reset back to default

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import * as React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+//
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#fff',
-      }}
-    ></View>
-  );
+  const [loaded] = useFonts({
+    Mulish: require('./assets/fonts/Mulish.ttf'),
+    MulishBold: require('./assets/fonts/MulishBold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <View style={styles.container}></View>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+    paddingVertical: 60,
+    justifyContent: 'space-between',
+  },
+});
